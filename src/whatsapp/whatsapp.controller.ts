1585653c0938e857efc7c2ca3c3a5c10abbe1c97
Kaveendra Yapaarachchi <kaveendra@jrcsoftware.com>
2025-05-11 15:08:04 +0530

    integrated with recipient

---
+++ src/whatsapp/whatsapp.controller.ts
@@ -0,0 +1,41 @@
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+
+@Controller('whatsapp')
+export class WhatsappController {
+
+    @Get('test')
+    test() {
+        return 'test chat';
+    }
+
+    @Get('webhook')
+        challengeWebhook(@Req() req, @Res() res) {
+            let mode = req.query["hub.mode"];
+            let token = req.query["hub.verify_token"];
+            let challenge = req.query["hub.challenge"];
+          
+            // Check if a token and mode is in the query string of the request
+            if (mode && token) {
+              // Check the mode and token sent is correct
+              if (mode === "subscribe" && token === '123456789') {
+                // Respond with the challenge token from the request
+                console.log("WEBHOOK_VERIFIED");
+                res.status(200).send(challenge);
+              } else {
+                // Respond with '403 Forbidden' if verify tokens do not match
+                res.sendStatus(403);
+              }
+            }
+          };
+
+
+          @Post('webhook')
+          handleWebhook(@Req() req, @Res() res) {
+            const body = req.body;
+            console.log('Received webhook:', body);
+          
+            // Respond with a 200 OK status
+            res.sendStatus(200);
+          }
+        }
+    
